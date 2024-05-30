be05797a070e7bb74e6b82db22fa41c0c1064f68
issam-seghir <issamusma@hotmail.com>
2024-05-31 00:46:20 +0100

    improve input component

--- src/app/components/input.tsx
+++ src/app/components/input.tsx
@@ -30,7 +30,9 @@ export function Input({
             <input id={id} type={type} autoComplete={id} disabled={disabled} {...register(id,{required})}
             className={clsx(
                 "form-input" ,
-                "block w-full rounded-md shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300",
+                "block w-full rounded-md border-0 py-1.5 text-gray-900  shadow-sm sm:text-sm ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-sky-600 sm:leading-6 ",
+                errors[id] && "focus:ring-rose-500" ,
+                disabled && "opacity-50 cursor-default"
 
             )}/>
 

69b718f1cb7c127f57a66dc7eb20ea1b14d77f1c
issam-seghir <issamusma@hotmail.com>
2024-05-31 11:39:52 +0100

    integrate prisma + mongodb

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -22,21 +22,24 @@ export function Input({
     disabled = false,
 }: InputProps) {
   return (
-    <div >
-        <label htmlFor={id} className='block text-sm font-medium leading-6 text-gray-900'>
-            {label}
-        </label>
-        <div className='mt-2'>
-            <input id={id} type={type} autoComplete={id} disabled={disabled} {...register(id,{required})}
-            className={clsx(
-                "form-input" ,
-                "block w-full rounded-md border-0 py-1.5 text-gray-900  shadow-sm sm:text-sm ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-sky-600 sm:leading-6 ",
-                errors[id] && "focus:ring-rose-500" ,
-                disabled && "opacity-50 cursor-default"
-
-            )}/>
-
-        </div>
-    </div>
-  )
+		<div>
+			<label htmlFor={id} className="block text-sm font-medium leading-6 text-gray-900">
+				{label}
+			</label>
+			<div className="mt-2">
+				<input
+					id={id}
+					type={type}
+					autoComplete={id}
+					disabled={disabled}
+					{...register(id, { required })}
+					className={clsx(
+						"form-input block w-full rounded-md border-0 py-1.5 text-gray-900  shadow-sm sm:text-sm ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-sky-600 sm:leading-6 ",
+						errors[id] && "focus:ring-rose-500",
+						disabled && "opacity-50 cursor-default"
+					)}
+				/>
+			</div>
+		</div>
+  );
 }

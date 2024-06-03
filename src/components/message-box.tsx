8ac2efab19fa264ee10686f262f2b43a49a256cf
issam-seghir <issamusma@hotmail.com>
2024-06-03 21:27:47 +0100

    add conversation messages styling

---
+++ src/components/message-box.tsx
@@ -0,0 +1,47 @@
+import { FullMessage } from "@/libs/types";
+import clsx from "clsx";
+import { format } from "date-fns";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { Avatar } from "./avatar";
+interface MessageBoxProps {
+	data: FullMessage;
+	isLast?: boolean;
+}
+
+export default function MessageBox({ data, isLast }: MessageBoxProps) {
+	const session = useSession();
+	const isOwn = data?.sender?.email === session.data?.user?.email;
+	const seenList = (data.seen || [])
+		.filter((user) => user.email !== data?.sender?.email)
+		.map((user) => user.name)
+		.join(", ");
+	const continer = clsx("flex gap-3 p-4", isOwn && "justify-end");
+	const avatar = clsx("rounded-full w-fit h-fit", isOwn && "order-2");
+	const body = clsx("flex flex-col gap-2 ", isOwn && "items-end");
+	const message = clsx(
+		"text-sm w-fit overflow-hidden",
+		isOwn ? "bg-sky-500 text-white" : "bg-gray-100",
+		data?.image ? "rounded-md p-0" : "rounded-full py-2 px-3"
+	);
+	return (
+		<div className={continer}>
+			<div className={avatar}>
+				<Avatar user={data.sender} />
+			</div>
+			<div className={body}>
+				<div className="flex items-baseline justify-center gap-1">
+					<div className="text-sm text-gray-500">{data.sender.name}</div>
+					<div className="text-xs text-gray-400">{format(new Date(data.createdAt), "p")}</div>
+					<div>{isOwn && <div className="text-xs text-gray-400">{seenList}</div>}</div>
+				</div>
+				<div className={message}>
+					{data.image && (
+						<Image className="rounded-md" src={data.image} width={150} height={150} alt="image" />
+					)}
+					{data.body}
+				</div>
+			</div>
+		</div>
+	);
+}

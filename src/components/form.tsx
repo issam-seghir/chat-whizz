d136965790944b8672caa42ccfd0fcef1a92c947
issam-seghir <issamusma@hotmail.com>
2024-06-03 19:27:42 +0100

    integrate to cloudinary

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,10 +1,10 @@
 "use client";
+import { CldUploadButton } from "next-cloudinary";
 
 import useConversation from "@/hooks/useConverstaion";
 import axios from "axios";
 import { FieldValues, useForm } from "react-hook-form";
-import { HiPhoto } from "react-icons/hi2";
-import { HiPaperAirplane } from "react-icons/hi2";
+import { HiPaperAirplane, HiPhoto } from "react-icons/hi2";
 import MessageInput from "./message-input";
 export function Form() {
 	const { conversationId } = useConversation();
@@ -21,16 +21,23 @@ export function Form() {
 	});
 
 	const onSubmit = (data: FieldValues) => {
-
 		setValue("message", "", { shouldValidate: true });
 		axios.post("/api/messages", {
 			...data,
 			conversationId,
 		});
 	};
+	const handleUpload = (result:any) => {
+		axios.post("/api/messages",{
+			image : result?.info?.secure_url,
+			conversationId
+		})
+	}
 	return (
 		<div className="py-4 px-4 bg-white border-t flex items-center gap-2 lg:gap-4 w-full">
-			<HiPhoto size={30} className="text-sky-500" />
+			<CldUploadButton options={{ maxFiles: 1 }} onSuccess={handleUpload} uploadPreset="fyjlpc4m">
+				<HiPhoto size={30} className="text-sky-500" />
+			</CldUploadButton>
 			<form onSubmit={handleSubmit(onSubmit)} className="flex items-center gap-2 lg:gap-4 w-full">
 				<MessageInput id="message" placeholder="Type a message" required register={register} errors={errors} />
 				<button

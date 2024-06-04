a8b14bfca80a4deda75ffec9734afd1c705862c9
issam-seghir <issamusma@hotmail.com>
2024-06-04 09:47:24 +0100

    fix drawer component closing style

--- src/components/profile-drawer.tsx
+++ src/components/profile-drawer.tsx
@@ -1,10 +1,11 @@
 "use client";
 import useOtherUser from "@/hooks/useOtherUser";
-import { Dialog, Transition, TransitionChild, DialogPanel } from "@headlessui/react";
-import {IoClose} from "react-icons/io5";
+import { Dialog, DialogPanel, Transition, TransitionChild } from "@headlessui/react";
 import { Conversation, User } from "@prisma/client";
 import { format } from "date-fns";
 import { Fragment, useMemo } from "react";
+import { IoClose } from "react-icons/io5";
+import { Avatar } from "./avatar";
 interface ProfileDrawerProps {
 	data: Conversation & {
 		users: User[];
@@ -35,7 +36,7 @@ export function ProfileDrawer({ data, isOpen, onClose }: ProfileDrawerProps) {
 	}, [data]);
 	return (
 		<Transition show={isOpen} as={Fragment}>
-			<Dialog as="div" className="relative z-50" onClose={onClose}>
+			<Dialog as="div" className="relative z-50" onClose={onClose} >
 				<TransitionChild
 					as="div"
 					enter="ease-out duration-500"
@@ -48,7 +49,8 @@ export function ProfileDrawer({ data, isOpen, onClose }: ProfileDrawerProps) {
 					<div className="fixed inset-0 bg-black bg-opacity-40" />
 					<div className="fixed inset-0 overflow-hidden">
 						<div className="absolute inset-0 overflow-hidden">
-							<div className="fixed pointer-events-none inset-y-0 right-0 flex max-w-full pl-10">
+							<div className="fixed
+                             inset-y-0 right-0 flex max-w-full pl-10">
 								<TransitionChild
 									as={Fragment}
 									enter="transform transition ease-in-out duration-500"
@@ -56,14 +58,15 @@ export function ProfileDrawer({ data, isOpen, onClose }: ProfileDrawerProps) {
 									leave="transform transition ease-in-out duration-500"
 									leaveTo="translate-x-full"
 								>
-									<DialogPanel className="pointer-events-none w-screen max-w-md">
+									<DialogPanel className="w-screen max-w-md">
 										<div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
 											<div className="px-4 sm:px-6">
 												<div className="flex items-start justify-end">
 													<div className="ml-3 flex h-7 items-center">
 														<button
+															onClick={onClose}
 															type="button"
-															className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:ring-2 focus:outline-none focus:ring-sky-500 focus:ring-offset-2"
+															className="rounded-md cursor-pointer bg-white text-gray-400 hover:text-gray-500 focus:ring-2 focus:outline-none focus:ring-sky-500 focus:ring-offset-2"
 														>
 															<span className="sr-only">Close panel</span>
 															<IoClose size={24} />
@@ -71,6 +74,15 @@ export function ProfileDrawer({ data, isOpen, onClose }: ProfileDrawerProps) {
 													</div>
 												</div>
 											</div>
+                                            <div className="relative mt-6 flex-1 px-4 sm:px-6">
+                                                <div className="flex flex-col items-center">
+                                                    <div className="mb-2">
+                                                        <Avatar user={otherUser} />
+                                                    </div>
+                                                    <div className="text-lg font-semibold">{title}</div>
+                                                    <div className="text-sm font-light text-neutral-500">{statusText}</div>
+                                                </div>
+                                            </div>
 										</div>
 									</DialogPanel>
 								</TransitionChild>

f15fa42ebd190facb8a7700a7be5855d54202c08
issam-seghir <issamusma@hotmail.com>
2024-06-05 15:43:01 +0100

    styling setting model

--- src/components/settings-model.tsx
+++ src/components/settings-model.tsx
@@ -1,16 +1,17 @@
 "use client";
 
 import { Button } from "@/components/button";
+import { Input } from "@/components/input";
 import useConversation from "@/hooks/useConverstaion";
-import { DialogTitle } from "@headlessui/react";
 import { User } from "@prisma/client";
 import axios from "axios";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
-import { FiAlertTriangle } from "react-icons/fi";
 import { Model } from "./model";
+import { CldUploadButton } from "next-cloudinary";
 
 interface SettingsModelProps {
 	isOpen?: boolean;
@@ -44,7 +45,7 @@ export function SettingsModel({ isOpen, onClose, currentUser }: SettingsModelPro
 		});
 	};
 
-	const onsSubmit: SubmitHandler<FieldValues> = async (data) => {
+	const onSubmit: SubmitHandler<FieldValues> = async (data) => {
 		try {
 			setIsLoading(true);
 			const response = await axios.post("/api/settings", data);
@@ -62,27 +63,55 @@ export function SettingsModel({ isOpen, onClose, currentUser }: SettingsModelPro
 
 	return (
 		<Model isOpen={isOpen} onClose={onClose}>
-			<div className="sm:flex sm:items-start">
-				<div className="mx-auto flex h-12 w-12 flex-shrink-0 rounded-full items-center justify-center bg-red-100 sm:mx-0 sm:h-10 sm:w-10 =">
-					<FiAlertTriangle className="h-6 w-6 text-red-600" />
-				</div>
-				<div className="text-center mt-3 sm:ml-4 sm:mt-0 sm:text-left">
-					<DialogTitle as="h3" className="text-base font-semibold leading-6 text-gray-900">
-						Delete Conversation
-					</DialogTitle>
-					<div className="mt-2">
-						<p className="text-sù text-gray-500">Are you sure you want to delete this conversation</p>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<div className="space-y-12">
+					<div className="border-b border-gray-900/10 pb-12">
+						<h2 className="text-base font-semibold leading-7 text-gray-900">Profile</h2>
+						<p className="mt-1 text-sm leading-6 text-gray-600"> Edit your public information</p>
+						<div className="mt-10 felx flex-col gap-y-8">
+							<Input
+								disabled={isLoading}
+								label="Name"
+								id="name"
+								errors={errors}
+								required
+								register={register}
+							/>
+						</div>
+						<div>
+							<label className="block leading-6 font-medium text-sm text-gray-900" htmlFor="">
+								Photo
+							</label>
+							<div className="mt-2 items-center gap-x-3">
+								<Image
+									alt="profile"
+									width="48"
+									height="48"
+									className="rounded-full"
+									src={image || currentUser?.image || "https://i.imgur.com/AdFtdpW.png"}
+								/>
+								<CldUploadButton
+									options={{ maxFiles: 1 }}
+									onSuccess={handleUpload}
+									uploadPreset="fyjlpc4m"
+								>
+									<Button  disabled={isLoading} secondary >
+										Change
+									</Button>
+								</CldUploadButton>
+							</div>
+						</div>
 					</div>
-				</div>
-			</div>
-			<div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-				<Button className="ml-4" disabled={isLoading} danger onClick={onDelete}>
-					Delete
-				</Button>
+			<div className="mt-6  flex items-center justify-end gap-x-6">
 				<Button disabled={isLoading} secondary onClick={onClose}>
 					Cancel
 				</Button>
+				<Button type="submit" disabled={isLoading} >
+					Save
+				</Button>
 			</div>
+				</div>
+			</form>
 		</Model>
 	);
 }

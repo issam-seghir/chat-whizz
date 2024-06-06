6abedc17740e6cae2df3fae0627e3c12b41d898c
issam-seghir <issamusma@hotmail.com>
2024-06-06 11:13:42 +0100

    integrate zustand + update readme

--- src/hooks/useActiveList.tsx
+++ src/hooks/useActiveList.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import {create} from "zustand"
 
-export default function useActiveList() {
-  return (
-    <div>useActiveList</div>
-  )
+interface ActiveListState {
+  members: string[];
+  set: (ids: string[]) => void;
+  add: (id: string) => void;
+  remove: (id: string) => void;
 }
+
+const useActiveList = create<ActiveListState>((set) => ({
+  members: [],
+  set: (ids) => set({ members: ids }),
+  add: (id) => set((state) => ({ members: [...state.members, id] })),
+  remove: (id) => set((state) => ({ members: state.members.filter((memberId) => memberId !== id) })),
+}));
+
+export default useActiveList

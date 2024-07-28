9c73293ce29da28a31bcf1b49c712d1c81e43eb7
issam-seghir <issamusma@hotmail.com>
2024-07-28 10:21:58 +0100

    improve create gorup modal styling

--- src/components/models/group-chat-model.tsx
+++ src/components/models/group-chat-model.tsx
@@ -65,8 +65,8 @@ export function GroupChatModel({ users, isOpen, onClose }: GroupChatModelProps) 
 			<form onSubmit={handleSubmit(onSubmit)}>
 				<div className="text-left space-y-12">
 					<div className="border-b border-gray-900/10 pb-12">
-						<h2 className="text-base font-semibold leading-7 text-gray-900">Create a Group Chat</h2>
-						<p className="mt-1 text-sm leading-6 text-gray-600"> Create a chat with more than 2 people </p>
+						<h2 className="text-base font-semibold leading-7 ">Create a Group Chat</h2>
+						<p className="mt-1 text-sm leading-6 "> Create a chat with more than 2 people </p>
 						<div className="mt-10 mb-4 flex flex-col gap-y-8">
 							<Input
 								disabled={isLoading}
--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { useTheme } from 'next-themes';
 import React from 'react';
 import ReactSelect from "react-select";
 interface SelectProps {
@@ -10,22 +11,43 @@ interface SelectProps {
 }
 
 const Select: React.FC<SelectProps> = ({ label, value, onChange, options, disabled }) => {
+    const themeData = useTheme()
+    const isDarkMode = themeData.theme === "dark";
+
 	return (
 		<div className="z-[100]">
-            <label className="block text-sm font-medium leading-6 text-gray-900">{label}</label>
-            <div className="mt-2">
-                <ReactSelect isDisabled={disabled} value={value} onChange={onChange} isMulti options={options} menuPortalTarget={document.body} styles={{
-                    menuPortal : (base) => ({
-                        ...base,
-                        zIndex:9999
-                    })
-                }}
-                classNames={{
-                    control: ()=> "text-sm"
-                }}></ReactSelect>
-            </div>
-
-        </div>
+			<label className="block text-sm font-medium leading-6">{label}</label>
+			<div className="mt-2">
+				<ReactSelect
+					isDisabled={disabled}
+					value={value}
+					onChange={onChange}
+					isMulti
+					options={options}
+					menuPortalTarget={document.body}
+					theme={(theme) => ({
+						...theme,
+						colors: isDarkMode
+							? {
+									...theme.colors,
+									neutral0: "#FFF5F", // Light mode color
+									primary25: "#141c39",
+									primary: "#080e24",
+							  }
+							: theme.colors, // Default colors for other modes
+					})}
+					styles={{
+						menuPortal: (base) => ({
+							...base,
+							zIndex: 9999,
+						}),
+					}}
+					classNames={{
+						control: () => "text-sm bg-white",
+					}}
+				></ReactSelect>
+			</div>
+		</div>
 	);
 };
 

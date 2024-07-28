3203f037ef52d0fa0cff18ca6b81711ced83127d
issam-seghir <issamusma@hotmail.com>
2024-07-28 22:36:15 +0100

    fix border for people page

--- src/components/user-list.tsx
+++ src/components/user-list.tsx
@@ -9,7 +9,7 @@ interface UserListProps {
 export function UserList({ items }: UserListProps) {
 
 	return (
-		<aside className="fixed inset-y-0 pb-20 lg:pb-0 lg:left-20 lg:w-80 lg:block overflow-y-auto border-r border-gray-200 block w-full left-0">
+		<aside className="fixed inset-y-0 pb-20 lg:pb-0 lg:left-20 lg:w-80 lg:block overflow-y-auto border-r  block w-full left-0">
 			<div className="px-5">
 				<div className="flex-col">
 					<div className="text-2xl font-bold  py-4">People</div>
